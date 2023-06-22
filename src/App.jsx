4f32a964344b1b6569dc955210a90c51fe43c8ef
Alessandro Benig <109065655+isipisii@users.noreply.github.com>
2023-06-22 12:22:31 +0800

    removed footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React from 'react'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import React from "react";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-   <main> 
+    <main>
       <Navbar />
       <Home />
-      <Footer/>
-   </main>
-  )
-}
+    </main>
+  );
+};
 
-export default App
+export default App;
